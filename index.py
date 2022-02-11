acc8b60f2c582656ccc4332817891725a7138ed3
Tobias Eikmeier <tobias_eikmeier@web.de>
2022-02-11 03:51:38 -0800

    feat: use ol wrapper eventing to implement game loop

--- index.py
+++ index.py
@@ -3,41 +3,53 @@ from ola.ClientWrapper import ClientWrapper
 import array
 import sys
 import time
+import logging
 
 wrapper = None
-
+client = None
+last_frame_time = 0
+universe = 1
+dmx_data = array.array('B', [0] * 512)
+FPS = 30
 
 def DmxSent(status):
   if status.Succeeded():
-    print('Success!')
+    logging.debug('Success!')
   else:
-    print('Error: %s' % status.message, file=sys.stderr)
+    logging.error('Error: %s' % status.message, file=sys.stderr)
 
+  # global wrapper
+  # if wrapper:
+  #   wrapper.Stop()
+
+
+def main_loop():
+
+  logging.debug('')
+  logging.debug('------------------------')
+
+  # Calculate and wait time delta until next frame can be started
+  global last_frame_time
+  now = time.time()
+  sleep_time = 1./FPS - (now - last_frame_time)
+  last_frame_time = now
+  if sleep_time > 0:
+    time.sleep(sleep_time)
+
+  # Main loop content
   global wrapper
-  if wrapper:
-    wrapper.Stop()
+  global client
+  global universe
+  global dmx_data
+  process_events() # process input and other stuff
+  update_state() # update all objects that need to be updated, e.g. position changes, physics, all that other stuff
+  render(client, universe, dmx_data) #render things on screen
 
+  logging.debug('------------------------')
+  logging.debug('')
 
-def main():
-  print_welcome()
-
-  running = True
-  FPS = 30
-  last_frame_time = 0
-
-  while running:
-    current_time = time.time()
-    sleep_time = 1./FPS - (current_time - last_frame_time)
-    last_frame_time = current_time
-    if sleep_time > 0:
-      time.sleep(sleep_time)
-
-    process_events() # process input and other stuff
-    update_state() # update all objects that need to be updated, e.g. position changes, physics, all that other stuff
-    render() #render things on screen
-
-    print('------------------------')
-    print('')
+  # Restart loop
+  wrapper.AddEvent(0, main_loop)
 
 def print_welcome():
   print('=======================')
@@ -47,31 +59,36 @@ def print_welcome():
   print('=======================')
 
 def process_events():
-  print('input processing')
-  print('event processing')
+  logging.debug('input processing')
+  logging.debug('event processing')
 
 def update_state():
-  print('update state')
+  logging.debug('update state')
+  dmx_data[0] = 255
+  dmx_data[2] = 255
+  dmx_data[8] = 255
+  dmx_data[14] = 255
+  dmx_data[20] = 255
+  dmx_data[26] = 255
+  dmx_data[32] = 255
+  dmx_data[38] = 255
+  dmx_data[44] = 255
+  dmx_data[50] = 255
+  dmx_data[56] = 255
+  dmx_data[62] = 255
+  dmx_data[68] = 255
 
-def render():
-  print('read state and send dmx')
 
-  universe = 1
-  data = array.array('B')
-  # append first dmx-value
-  data.append(255)
-  # append second dmx-value
-  data.append(0)
-  # append third dmx-value
-  data.append(255)
 
-  global wrapper
-  wrapper = ClientWrapper()
-  client = wrapper.Client()
-  # send 1 dmx frame with values for channels 1-3
-  client.SendDmx(universe, data, DmxSent)
-  wrapper.Run()
-
+def render(client, universe, dmx_data):
+  logging.debug('read state and send dmx')
+  client.SendDmx(universe, dmx_data, DmxSent)
 
 if __name__ == '__main__':
-  main()
+  print_welcome()
+
+  wrapper = ClientWrapper()
+  client = wrapper.Client()
+  wrapper.AddEvent(0, main_loop)
+  wrapper.Run()
+
