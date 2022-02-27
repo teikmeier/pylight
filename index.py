8c9fd29816a9a44a436570883f4d25fab6b87326
Tobias Eikmeier <tobias_eikmeier@web.de>
2022-02-27 04:09:16 -0800

    fix: reorder and cleanup methods

--- index.py
+++ index.py
@@ -6,51 +6,19 @@ import sys
 import time
 import logging
 
+# Constants, startup options, and globals
 wrapper = None
 client = None
-last_frame_time = 0
+last_frame_date = 0
 universe = 1
 dmx_data = array.array('B', [DMX_MIN_SLOT_VALUE] * DMX_UNIVERSE_SIZE)
 FPS = 30
 
-def DmxSent(status):
-  if status.Succeeded():
-    logging.debug('Success!')
-  else:
-    logging.error('Error: %s' % status.message, file=sys.stderr)
-
-  # global wrapper
-  # if wrapper:
-  #   wrapper.Stop()
-
-
-def main_loop():
-
-  logging.debug('')
-  logging.debug('------------------------')
-
-  # Calculate and wait time delta until next frame can be started
-  global last_frame_time
-  now = time.time()
-  sleep_time = 1./FPS - (now - last_frame_time)
-  last_frame_time = now
-  if sleep_time > 0:
-    time.sleep(sleep_time)
-
-  # Main loop content
-  global wrapper
-  global client
-  global universe
-  global dmx_data
-  process_events() # process input and other stuff
-  update_state() # update all objects that need to be updated, e.g. position changes, physics, all that other stuff
-  render(client, universe, dmx_data) #render things on screen
-
-  logging.debug('------------------------')
-  logging.debug('')
-
-  # Restart loop
-  wrapper.AddEvent(0, main_loop)
+def main():
+  print_welcome()
+  read_config()
+  load_show()
+  start_wrapper()
 
 def print_welcome():
   print('=======================')
@@ -60,6 +28,52 @@ def print_welcome():
   print('=======================')
   print()
   print()
+
+def read_config():
+  logging.debug('cli options')
+
+def load_show():
+  logging.debug('loading show')
+  logging.debug('show loaded')
+
+def start_wrapper():
+  global wrapper
+  global client
+  wrapper = ClientWrapper()
+  client = wrapper.Client()
+  wrapper.AddEvent(0, main_loop)
+  wrapper.Run()
+
+def main_loop():
+
+  logging.debug('')
+  logging.debug('------------------------')
+
+  # Calculate and wait time delta until next frame can be started
+  global last_frame_date
+  now = time.time()
+  sleep_time = 1./FPS - (now - last_frame_date)
+  last_frame_date = now
+  if sleep_time > 0:
+    time.sleep(sleep_time)
+
+  # Main loop content
+  global wrapper
+  global client
+  global universe
+  global dmx_data
+  process_events() # process input and other stuff
+  update_state() # update all objects that need to be updated, e.g. scene changes, scene updates
+  render(client, universe, dmx_data) # send things to DMX
+
+  logging.debug('------------------------')
+  logging.debug('')
+
+  # Restart loop
+  wrapper.AddEvent(0, main_loop)
+
+
+# ----------------- Loop steps -----------------
 
 def process_events():
   logging.debug('input processing')
@@ -83,13 +97,13 @@ def update_state():
 
 def render(client, universe, dmx_data):
   logging.debug('read state and send dmx')
-  client.SendDmx(universe, dmx_data, DmxSent)
+  client.SendDmx(universe, dmx_data, __on_dmx_sent)
+
+def __on_dmx_sent(status):
+  if status.Succeeded():
+    logging.debug('Success!')
+  else:
+    logging.error('Error: %s' % status.message, file=sys.stderr)
 
 if __name__ == '__main__':
-  print_welcome()
-
-  wrapper = ClientWrapper()
-  client = wrapper.Client()
-  wrapper.AddEvent(0, main_loop)
-  wrapper.Run()
-
+  main()
